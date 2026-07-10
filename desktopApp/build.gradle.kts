576cec063f22c77d65c8cb6380588befb2a14a89
CiroFeliu <ricardociro97@gmail.com>
2026-07-11 00:30:24 +0200

    Fix version name error. Update gradle.

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -34,7 +34,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "app.luxion.shogunai"
-            packageVersion = (findProperty("appVersion") as String?) ?: "0.0.0-dev"
+            packageVersion = (findProperty("appVersion") as String?) ?: "0.0.0"
         }
     }
 }
